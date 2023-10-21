030d9fa25793179557adeb78e591fc9ed09eae0d
DanielaQuintas <pqdaniela@hotmail.com>
2023-10-21 18:58:56 +0100

    bosta

--- cliente.py
+++ cliente.py
@@ -1,9 +1,20 @@
 import socket
-cliente= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
-    mensagem_envio= print("HELLO")
-    cliente.sendto(mensagem_envio.encode("10.0.0.10"),3000)
-    mensagem_bytes, endereco_ip_servidor= cliente.recvfrom(2048)
-    print(mensagem_bytes.decode())
+# Defina o endereço IP e a porta do servidor
+host = 'IP_DO_SERVIDOR'  # Substitua pelo IP do seu servidor
+port = PORTA_DO_SERVIDOR  # Substitua pela porta usada pelo servidor
 
+# Crie um socket do cliente
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conecte-se ao servidor
+client_socket.connect((host, port))
+
+# Receba a mensagem "HELLO" do servidor
+message = client_socket.recv(1024)
+
+# Imprima a mensagem recebida
+print(f"Recebido do servidor: {message.decode()}")
+
+# Feche a conexão com o servidor
+client_socket.close()
--- servidor.py
+++ servidor.py
@@ -1,8 +1,29 @@
 import socket
 
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind(('', 3000))
+# Defina o endereço IP e a porta do servidor
+host = '10.0.0.10'  # Substitua pelo IP do seu servidor
+port = 3000  # Substitua pela porta desejada
 
-while True:
-    mensagem_bytes,endereco_ip_cliente = servidor.recvfrom(2048)
-    servidor.sendto("Hello".encode().endereco_ip_cliente)
+# Crie um socket do servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Associe o socket ao endereço e à porta
+server_socket.bind((host, port))
+
+# Espere por uma conexão
+server_socket.listen(1)
+
+print(f"Servidor aguardando conexão em {host}:{port}")
+
+# Aceite a conexão quando um cliente se conectar
+client_socket, client_address = server_socket.accept()
+
+# Envie a mensagem "HELLO" para o cliente
+message = "HELLO"
+client_socket.send(message.encode())
+
+# Feche a conexão com o cliente
+client_socket.close()
+
+# Feche o socket do servidor
+server_socket.close()
