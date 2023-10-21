c7255a954374f10d2b29292ac959d2da2fd21dfa
DanielaQuintas <pqdaniela@hotmail.com>
2023-10-21 19:54:23 +0100

    estou só a ver

--- cliente.py
+++ cliente.py
@@ -1,19 +1,27 @@
 import socket
-# Definir o endereço IP e a porta do servidor
-host = '10.0.0.10'  # Substituir pelo IP do servidor
-port = 10000  
 
-# Criar um socket do cliente
+# Defina o endereço IP e a porta do servidor
+host = '10.0.0.10'  # Substitua pelo IP do servidor
+port = 10000  # Substitua pela porta usada pelo servidor
+
+# Crie um socket do cliente
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Conectar ao servidor
+# Conecte-se ao servidor
 client_socket.connect((host, port))
 
-# Receber a mensagem "HELLO" do servidor
-message = client_socket.recv(1024)
+while True:
+    # Envie uma mensagem para o servidor
+    message = "Olá, servidor!"
+    client_socket.send(message.encode())
 
-# Imprimir a mensagem recebida
-print(f"Recebido do servidor: {message.decode()}")
+    # Receba uma resposta do servidor
+    response = client_socket.recv(1024)
+    if not response:
+        break  # Se não houver mais respostas do servidor, saia do loop
+
+    # Imprimir a resposta recebida do servidor
+    print(f"Recebido do servidor: {response.decode()}")
 
 # Feche a conexão com o servidor
 client_socket.close()
--- servidor.py
+++ servidor.py
@@ -1,13 +1,13 @@
 import socket
 
-# Definir o endereço IP e a porta do servidor
-host = '10.0.0.10'  # Substituir pelo IP do servidor
+# Defina o endereço IP e a porta do servidor
+host = '10.0.0.10'  # Substitua pelo IP do servidor
 port = 10000  # Substitua pela porta desejada
 
-# Criar um socket do servidor
+# Crie um socket do servidor
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Associar o socket ao endereço e à porta
+# Associe o socket ao endereço e à porta
 server_socket.bind((host, port))
 
 # Espere por uma conexão
@@ -15,12 +15,21 @@ server_socket.listen(1)
 
 print(f"Servidor aguardando conexão em {host}:{port}")
 
-# Aceitar a conexão quando um cliente se conectar
+# Aceite a conexão quando um cliente se conectar
 client_socket, client_address = server_socket.accept()
 
-# Envie a mensagem "HELLO" para o cliente
-message = "HELLO"
-client_socket.send(message.encode())
+while True:
+    # Receba uma mensagem do cliente
+    message = client_socket.recv(1024)
+    if not message:
+        break  # Se não houver mais mensagens do cliente, saia do loop
+
+    # Processar a mensagem recebida (aqui, apenas a imprime)
+    print(f"Recebido do cliente: {message.decode()}")
+
+    # Envie uma resposta de volta ao cliente
+    response = "Mensagem recebida pelo servidor"
+    client_socket.send(response.encode())
 
 # Fechar a conexão com o cliente
 client_socket.close()
