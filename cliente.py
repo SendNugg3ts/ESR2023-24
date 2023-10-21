70b43ed91a8ee51945df78e0d0e4d95cee619403
DanielaQuintas <pqdaniela@hotmail.com>
2023-10-21 19:40:08 +0100

    nova_versao

--- cliente.py
+++ cliente.py
@@ -1,19 +1,18 @@
 import socket
+# Definir o endereço IP e a porta do servidor
+host = '10.0.0.10'  # Substituir pelo IP do servidor
+port = 10000  
 
-# Defina o endereço IP e a porta do servidor
-host = '10.0.0.10'  # Substitua pelo IP do seu servidor
-port = 10000  # Substitua pela porta usada pelo servidor
-
-# Crie um socket do cliente
+# Criar um socket do cliente
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Conecte-se ao servidor
+# Conectar ao servidor
 client_socket.connect((host, port))
 
-# Receba a mensagem "HELLO" do servidor
+# Receber a mensagem "HELLO" do servidor
 message = client_socket.recv(1024)
 
-# Imprima a mensagem recebida
+# Imprimir a mensagem recebida
 print(f"Recebido do servidor: {message.decode()}")
 
 # Feche a conexão com o servidor
