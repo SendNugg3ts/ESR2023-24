4b1e51b97bef886f698ec49c813be37290c5c982
DanielaQuintas <pqdaniela@hotmail.com>
2023-10-21 18:31:32 +0100

    novamente

--- servidor.py
+++ servidor.py
@@ -1,3 +1,8 @@
 import socket
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind(('', 3000))
+
+while True:
+    mensagem_bytes,endereco_ip_cliente = servidor.recvfrom(2048)
+    
