e93242d6b920bb958ecea50f402315b4c7358663
DanielaQuintas <pqdaniela@hotmail.com>
2023-11-09 15:28:23 +0000

    adicionei router

--- src/oNode.py
+++ src/oNode.py
@@ -20,9 +20,17 @@ i = open(path_to_nodeID)
 
 info = json.load(i)
 
-nodeIP = info["ip"]
-isServer = bool(info["server"])
-hostIP = info["vizinhos"][0]["ip"]
+if bool(info["RP"]) == True:
+    nodeIP = info["ip"]
+    server1IP = info["vizinhos"][0]["ip"]
+    server2IP = info["vizinhos"][1]["ip"]
+    nodePort = info["porta"]
+elif bool(info["RP"]) == False and bool(info["router"]) == True:
+    nodeIP = info["ip"]
+    cliente1IP = info["vizinhos"][0]["ip"]
+    server2IP = info["vizinhos"][1]["ip"]
+    nodePort = info["porta"]
+
 
 filename = "movie.Mjpeg"
 SERVERPORT = 2500
@@ -31,33 +39,9 @@ if isServer:
 else:
     clientGuiStart(hostIP, SERVERPORT, nodeIP, 3500, nodeID, filename)
 
-if not isServer:
-    with open(f"{nodeID}.json", "r") as json_file:
-        cliente_json = json.load(json_file)
-        #atualizar_medicoes_latencia(cliente_json, nodeID)
-        with open(f"{nodeID}.json", "w") as json_file:
-            json.dump(cliente_json, json_file)
 
 
-def medir_latencia(vizinho):
-    try:
-        start_time = time.time()
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.settimeout(1)  # Defina um tempo limite para a resposta
-            s.connect((vizinho["ip"], vizinho["porta"]))
-            s.send(b"PING")  # Envie um pacote de ping
-            s.recv(1024)  # Aguarde a resposta
-        end_time = time.time()
-        latencia = (end_time - start_time) * 1000  # Converta para milissegundos
-        return latencia
-    except Exception as e:
-        return None
 
-def atualizar_medicoes_latencia(cliente_json, nodeID):
-    if not isServer:  # Somente para clientes
-        for vizinho in cliente_json["vizinhos"]:
-            latencia = medir_latencia(vizinho)
-            if latencia is not None:
-                vizinho["latencia"] = latencia
+
 
 
