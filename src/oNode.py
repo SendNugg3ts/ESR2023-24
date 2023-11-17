3edcfb3428f18b745ae478d5412a60de78299f77
SendNugg3ts <luis.fmsilva@hotmail.com>
2023-11-17 17:38:00 +0000

    cliente router ta top

--- src/oNode.py
+++ src/oNode.py
@@ -22,13 +22,20 @@ i = open(path_to_nodeID)
 
 info = json.load(i)
 
-if bool(info["RP"]) != True:
+if bool(info["RP"]) != True and bool(info["router"]) == False:
     nodeIP = info["ip"]
+
 elif bool(info["RP"]) == True:
     nodeIP1 = info["ip1"]
     nodeIP2 = info["ip2"]
     nodeIP3 = info["ip3"]
     nodeIP4 = info["ip4"]
+
+else:
+    nodeIP1 = info["ip1"]
+    nodeIP2 = info["ip2"]
+    nodeIP3 = info["ip3"]
+    
 
 nodePort_streaming = info["porta_streaming"]
 nodePort1_mensagem = info["porta_mensagem"]
@@ -50,60 +57,54 @@ def RPConfirmation(server_socket):
         
 
 
-def RpTestLatency(host1, port1_mensagem, port1_streaming, host2, port2_mensagem, port2_streaming, rpPort_streaming):
-    while True:
-        try:
-            # Create a socket for server 1
-            client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            start = time.time()
-            client_socket1.connect((host1, port1_mensagem))
-            message = "Time test"
-            client_socket1.send(message.encode())
+def RpTestLatency(host1, port1_mensagem, host2, port2_mensagem):
+    try:
+        client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        start = time.time()
+        client_socket1.connect((host1, port1_mensagem))
+        message = "Time test"
+        client_socket1.send(message.encode())
 
-            response = client_socket1.recv(1024)
-            print(f"Received from {host1}: {response.decode()}")
-            end = time.time()
-            latencia1 = end - start
-            client_socket1.close()
-        except Exception as e:
-            print(f"Error with server 1: {e}")
-            latencia1 = float('inf')  # Set a high latency value in case of an error
+        response = client_socket1.recv(1024)
+        print(f"Received from {host1}: {response.decode()}")
+        end = time.time()
+        latencia1 = end - start
+        client_socket1.close()
+    except Exception as e:
+        print(f"Error with path 1: {e}")
+        latencia1 = float('inf')  # Set a high latency value in case of an error
 
-        try:
-            # Create a socket for server 2
-            client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            start = time.time()
-            client_socket2.connect((host2, port2_mensagem))
-            message = "Time test"
-            client_socket2.send(message.encode())
+    try:
+        client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        start = time.time()
+        client_socket2.connect((host2, port2_mensagem))
+        message = "Time test"
+        client_socket2.send(message.encode())
 
-            response = client_socket2.recv(1024)
-            print(f"Received from {host2}: {response.decode()}")
-            end = time.time()
-            latencia2 = end - start
-            client_socket2.close()
-        except Exception as e:
-            print(f"Error with server 2: {e}")
-            latencia2 = float('inf')  # Set a high latency value in case of an error
-            
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        response = client_socket2.recv(1024)
+        print(f"Received from {host2}: {response.decode()}")
+        end = time.time()
+        latencia2 = end - start
+        client_socket2.close()
+    except Exception as e:
+        print(f"Error with path 2: {e}")
+        latencia2 = float('inf')  # Set a high latency value in case of an error
+        
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        if latencia1 < latencia2:
-            print("Server 1 is faster")
-            server_socket.connect((host1, port1_mensagem))
-            server_socket.send("START_STREAM".encode())
-            StartStreaming()
-            #clientGuiStart(host1,port1_streaming,nodeIP1,rpPort_streaming,nodeID,filename)#4010 tem de ser a porta de streaming do server e 3000 tem de ser a porta que recebe os rtsps
-        else:
-            print("Server 2 is faster")
-            server_socket.connect((host2, port2_mensagem)) 
-            server_socket.send("START_STREAM".encode())
-
-            #clientGuiStart(host2,port2_streaming,nodeIP2,rpPort_streaming,nodeID,filename)          
-        #print(f"Latency for server 1: {latencia1} seconds")
-        #print(f"Latency for server 2: {latencia2} seconds")
+    if latencia1 < latencia2:
+        print("Path 1 is faster")
+        server_socket.connect((host1, port1_mensagem))
+        server_socket.send("START_STREAM".encode())
         server_socket.close()
-        time.sleep(20)
+        return host1
+    else:
+        print("Path 2 is faster")
+        server_socket.connect((host2, port2_mensagem)) 
+        server_socket.send("START_STREAM".encode())
+        server_socket.close()
+        return host2
+
 
 
 
@@ -131,42 +132,34 @@ def mensagem_cliente_router(routerIP,routerPort):
     RPIP = cliente_socket.recv(1024).decode()
     return RPIP
 
-
-
-
-
-def mensagem_router_cliente(clienteIP,clientePorta):
+def mensagem_router_cliente():
     router_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     router_socket.bind((nodeIP3, nodePort1_mensagem))
-    server_socket.listen(5)
+    router_socket.listen(5)
     print(f"Router aguardando conexões em {nodeIP3}:{nodePort1_mensagem}")
     while True:
         client_socket, client_address = router_socket.accept()
         print(f"Conexão aceite de {client_address}")
         mensagem_cliente = client_socket.recv(1024).decode('utf-8')
         print(f"Mensagem recebida: {mensagem_cliente}")
-    melhor_caminho = RpTestLatency(vizinhos_ip[2],vizinhos_porta_mensagem[2],vizinhos_porta_streaming[2],vizinhos_ip[3],vizinhos_porta_mensagem[3] ,vizinhos_porta_streaming[3], rpPort_streaming)
-        ###ver como vejo qual é o melhor 
-        resposta_para_cliente = vizinhos_ip[]
-        client_socket.send(resposta_para_cliente.encode('utf-8'))
+        fasterIpRP=RpTestLatency(vizinhos_ip[2],vizinhos_porta_mensagem[2],vizinhos_ip[3],vizinhos_porta_mensagem[3])
+        client_socket.send(fasterIpRP.encode('utf-8'))
         client_socket.close()
+        break
 
 
 if bool(info["server"]) == True:#Servidor
     nodeType= "server"
     serverStart(nodeIP,nodePort1_mensagem,nodePort_streaming)
 
-elif bool(info["router"]) == False and bool(info["server"]) == False#Cliente cabou
+elif bool(info["router"]) == False and bool(info["server"]) == False:#Cliente cabou
     RPIP=mensagem_cliente_router(vizinhos_ip[0],vizinhos_porta_mensagem[0])
     clientGuiStart(RPIP, 3000, nodeIP, nodePort_streaming, nodeID, filename)
     
 
 elif bool(info["RP"]) == False and bool(info["router"]) == True:  # Router
     nodeType = "router"
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    #fica à escura
-
+    mensagem_router_cliente()
 
 elif bool(info["RP"]) == True:  # RP
     nodeType = "RP"
